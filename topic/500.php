670ef60a26339e0605952a93530a0ee8b1f3c4ab
Chiang1002 <125775190+Chiang1002@users.noreply.github.com>
2023-02-28 11:29:56 +0800

    Update 500.php

--- topic/500.php
+++ topic/500.php
@@ -9,7 +9,7 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>404 Error - SB Admin</title>
+        <title>行前訓練管理系統-訓練資訊</title>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     </head>
@@ -18,7 +18,7 @@ session_start();
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.php">行前訓練管理系統</a>
+            <a class="navbar-brand ps-3" href="index2.php">行前訓練管理系統</a>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 <div class="input-group">
@@ -42,11 +42,11 @@ session_start();
                     <div class="sb-sidenav-menu">
                         <div class="nav">                            
                             <div class="sb-sidenav-menu-heading">項目編輯</div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <a class="nav-link" href="#">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 訓練主體                               
                             </a>                       
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                            <a class="nav-link" href="500.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
                                 訓練資訊
                             </a>
@@ -87,10 +87,10 @@ session_start();
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
-                                教學
+                                教學資訊
                             </div>
                             <div class="card-body">
-                                <table id="datatablesSimple">
+                                <table class="table table-striped">
                                     <thead>
                                         <tr>
                                             <th></th>
@@ -133,11 +133,11 @@ session_start();
                                                         <td><?= $teaching['question']; ?></td>
                                                         <td><?= $teaching['solution']; ?></td>
                                                         <td>
-                                                            <a href="edit.php?id=<?= $teaching['id']; ?>" class="btn btn-success btn-sm">Edit</a>
+                                                            <a href="edit_teaching.php?id=<?= $teaching['id']; ?>" class="btn btn-success btn-sm">Edit</a>
                                                         </td>
-                                                        <td>
-                                                            <form action="code.php" method="POST" class="d-inline">
-                                                                <button type="submit" name="delete_student" value="<?=$teaching['id'];?>" class="btn btn-danger btn-sm">Delete</button>
+                                                        <td>                                         <!--在同一行-->
+                                                            <form action="receive.php" method="POST" class="d-inline">
+                                                                <button type="submit" name="delete_teaching" value="<?=$teaching['id'];?>" class="btn btn-danger btn-sm">Delete</button>
                                                             </form>
                                                         </td>
                                                     </tr>
@@ -202,11 +202,11 @@ session_start();
                                                         <td><?= $notice['question']; ?></td>
                                                         <td><?= $notice['solution']; ?></td>
                                                         <td>
-                                                            <a href="edit.php?id=<?= $notice['id']; ?>" class="btn btn-success btn-sm">Edit</a>
+                                                            <a href="edit_notice.php?id=<?= $notice['id']; ?>" class="btn btn-success btn-sm">Edit</a>
                                                         </td>
                                                         <td>
-                                                            <form action="code.php" method="POST" class="d-inline">
-                                                                <button type="submit" name="delete_student" value="<?=$notice['id'];?>" class="btn btn-danger btn-sm">Delete</button>
+                                                            <form action="receive.php" method="POST" class="d-inline">
+                                                                <button type="submit" name="delete_notice" value="<?=$notice['id'];?>" class="btn btn-danger btn-sm">Delete</button>
                                                             </form>
                                                         </td>
                                                     </tr>
